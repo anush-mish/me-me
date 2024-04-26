9c8a586a3adcc5fad1c24f71f2cc33eaa97e0357
Anushka Mishra <165766043+anush-mish@users.noreply.github.com>
2024-04-26 07:27:02 +0530

    Add files via upload
    js besty else tatti

---
+++ let_cost.js
@@ -0,0 +1,24 @@
+const accountId=144553;
+let email="abc123@gmail.com";
+var accountPassword="12345";
+accountCity="Dhanbad";
+
+//accountId=1234  // this is not allowed in node js.
+
+console.log(accountId);
+email="def@gmail.com";
+accountPassword="789";
+console.log(email);
+console.log(accountPassword);
+console.log(accountCity);
+accountCity="Patna";
+console.log(accountCity);
+
+
+let accountState;
+console.log(accountState);
+//js will consider it undefined
+
+console.log([accountId,email,accountPassword,accountCity]);
+
+/*new type of commenting style */
